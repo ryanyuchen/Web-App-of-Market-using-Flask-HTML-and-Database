49118d1b45f2af578c9569a9a646e34eb8acd327
ryanyuchen <vanchen10@gmail.com>
2021-12-22 13:21:11 -0500

    make market as library package

--- market/__init__.py
+++ market/__init__.py
@@ -1,3 +1,7 @@
+'''
+__init__.py make the market folder as the library package and
+can be imported as "from market import db" in run.py
+'''
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -11,4 +15,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
+
+# import routes
 from market import routes
