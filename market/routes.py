11e5fb8eaba20d0f30f18b4fec9ab581c0182c45
ryanyuchen <vanchen10@gmail.com>
2021-12-22 12:21:22 -0500

    add db

--- market/routes.py
+++ market/routes.py
@@ -5,6 +5,36 @@ from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
+'''
+To create the db
+1) in python file:
+
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'   # return the default of Item with name
+
+2) in shell:
+
+from market import db
+db.create_all()
+from market import Item
+item1 = Item(name="", price=""....)
+db.session.add(item1)
+db.session.commit()
+Item.query.all()
+'''
+
 @app.route('/')
 @app.route('/home')
 def home_page():
